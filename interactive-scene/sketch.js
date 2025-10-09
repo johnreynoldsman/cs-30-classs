bc5ad421723674c2b8f55c20a5fada8e085fbd2a
johnreynoldsman <laconreynolds@gmail.com>
2025-10-09 09:51:57 -0600

    a basic concept
    not quite done yet

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,11 +1,33 @@
-//interactive scene
-
-
+let r = 0;
+let g = 0;
+let b = 0;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+  createCanvas(width, height);
 
+}
+//not done yet
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 50);
+  let mycolor = (r, g, b);
+  background(r, g, b);
+  if (keyIsDown(82)){
+    r += 1;
+  }
+  if (keyIsDown(84)){
+    r -= 1;
+  }
+  if (keyIsDown(71)){
+    g += 1;
+  }
+  if (keyIsDown(72)){
+    g -= 1;
+  }
+  if (keyIsDown(66)){
+    b += 1;
+  }
+  if (keyIsDown(78)){
+    b -= 1;
+  }
+  
+  
+  
 }
