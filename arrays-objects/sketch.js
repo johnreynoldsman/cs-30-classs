59ea38f181e36898e2b8f1c71fef9333f87327da
johnreynoldsman <laconreynolds@gmail.com>
2025-10-21 09:53:19 -0600

    figured out keypressed

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -9,13 +9,20 @@ let theColor = 'white';
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
-function draw() {
-  background(theColor);
-  // use keytyped or maybe keyisdown to add letters to an array thats in a textblock except that may only work by writing backwards
-  
+function keyPressed(){
+  if (keyCode === 65){
+    myarray.push('a');
+  }
 }
 
 
 
+function draw() {
+  background(theColor);
+  text(myarray , 100, 100);
+  // use keytyped or maybe keyisdown to add letters to an array thats in a textblock except that may only work by writing backwards
+  // use backspace to pop current 
 
+  
+  
+}
