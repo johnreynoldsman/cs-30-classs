e905ad23f7c48eaf8b215346d8aa3a138f65562e
johnreynoldsman <laconreynolds@gmail.com>
2025-11-07 09:54:11 -0600

    i think im done not 100% sure though

--- grid-project/sketch.js
+++ grid-project/sketch.js
@@ -7,10 +7,16 @@
 
 
 const CELL_SIZE = 20;
-const open_tile = 0;
-const wall = 1;
-const redsquare = 5;
-const bluesquare = 3;
+const whiteSquare = 0;
+const blackSquare = 1;
+const redSquare = 2;
+const blueSquare = 3;
+const brownSquare = 4;
+const yellowSquare = 5;
+const tanSquare = 6;
+const purpleSquare = 7;
+const orangeSquare = 8;
+const greenSquare = 9;
 let grid;
 let rows;
 let cols;
@@ -23,21 +29,18 @@ function setup() {
 }
 
 function draw() {
-  background("blue");
+  background(0);
   displayGrid();
 }
+function mousePressed() {
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+
+  toggleCell(x ,y);
+}
 function toggleCell(x, y) {
-  if (x >= 0 && x < cols && y >= 0 && y < rows) {
-    if (grid[y][x] === open_tile) {
-      grid[y][x] = wall;
-    }
-    else if (grid[y][x] === wall) {
-      grid[y][x] = open_tile;
-    }
-    else if (grid[y][x] === open_tile) {
-      grid[y][x] = redsquare;
-    }
-  }
+  grid[y][x] = blackSquare;
+    
 }
 
 function keyPressed() {
@@ -47,33 +50,68 @@ function keyPressed() {
     grid = generateEmptyGrid(cols, rows);
   }
   else if (key === "r"){
-    if (grid[y][x] === open_tile || wall) {
-      grid[y][x] = redsquare;
-    }
+    grid[y][x] = redSquare;
+    
   }
   else if (key === "b"){
-    if (grid[y][x] === open_tile || redsquare) {
-      grid[y][x] = wall;
-    }
+    grid[y][x] = blueSquare;
+    
   }
   else if (key === "w"){
-    if (grid[y][x] === redsquare || wall) {
-      grid[y][x] = open_tile;
-    }
+    grid[y][x] = whiteSquare;
+  }
+  else if (key === "y"){
+    grid[y][x] = yellowSquare;
+  }
+  else if (key === "t"){
+    grid[y][x] = tanSquare;
+  }
+  else if (key === "p"){
+    grid[y][x] = purpleSquare;
+  }
+  else if (key === "g"){
+    grid[y][x] = greenSquare;
+  }
+  else if (key === "o"){
+    grid[y][x] = orangeSquare;
+  }
+  else if (key === " "){
+    grid[y][x] = brownSquare;
   }
 }
 
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === open_tile) {
+      if (grid[y][x] === whiteSquare) {
         fill("white");
       }
-      else if (grid[y][x] === wall) {
+      else if (grid[y][x] === blackSquare) {
         fill("black");
       }
-      else if (grid[y][x] === redsquare) {
+      else if (grid[y][x] === redSquare) {
         fill("red");
+      }
+      else if (grid[y][x] === blueSquare){
+        fill ("blue");
+      }
+      else if (grid[y][x] === brownSquare){
+        fill ("#643200");
+      }
+      else if (grid[y][x] === yellowSquare){
+        fill ("yellow");
+      }
+      else if (grid[y][x] === tanSquare){
+        fill ("#f0cd95");
+      }
+      else if (grid[y][x] === greenSquare){
+        fill ("green");
+      }
+      else if (grid[y][x] === orangeSquare){
+        fill ("orange");
+      }
+      else if (grid[y][x] === purpleSquare){
+        fill ("purple");
       }
       square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
@@ -85,7 +123,7 @@ function generateEmptyGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      newGrid[y].push(open_tile);
+      newGrid[y].push(whiteSquare);
     }
   }
   return newGrid;
